6fba06baf6cad08af1fb4c1a61cd983ec1e284f2
mmuhammad0 <130362851+mmuhammad0@users.noreply.github.com>
2025-04-19 15:33:55 +0200

    Delete projects directory

--- projects/project10_kubernetes_deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-sudo apt update
-sudo apt install nginx
-sudo systemctl restart nginx
-sudo systemctl status nginx
-sudo certbot --nginx -d biuldwithme.link -d www.buildwithme.link
-
-# Images:
--- projects/project11_jenkins_pipeline.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-sudo apt update
-sudo apt install ansible
-git status
-git add <selected files>
-git commit -m "commit message"
-
-# Images:
